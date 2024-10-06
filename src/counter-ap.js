1e5369a322e88e340df340d3b83db3a67b227946
Krittanat Kulsakdinun <guykrittanat810@gmail.com>
2024-10-06 11:49:37 -0400

    first commit

--- src/counter-ap.js
+++ src/counter-ap.js
@@ -10,12 +10,61 @@ export class counterAp extends DDDSuper(LitElement) {
   constructor() {
     super();
     this.title = "";
+    this.counter = 0;
+    this.max = 0;
+    this.min = 0;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      counter: {type: Number, reflect: true},
+      max: {type: Number, reflect: true},
+      min: {type: Number, reflect: true},
     };
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('counter')) {
+      this.updateColor();
+      if (this.counter === 21) {
+        this.makeItRain();
+      }
+    }
+  }
+
+  updateColor() {
+    if (this.counter === this.min || this.counter === this.max) {
+      this.style.color = 'black';
+    }
+    else if (this.counter === 18){
+      this.style.color = "brown";
+    }
+    else if (this.counter === 21){
+      this.style.color = "blue";
+    }
+   else {
+      this.style.color = '';
+    }
+  }
+
+  increase() {
+    if (this.counter < this.max)
+        this.counter++;
+  }
+
+  decrease() {
+    if (this.counter > this.min)
+        this.counter--;
+  }
+
+  makeItRain() {
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+      setTimeout(() => {
+        document.querySelector("#confetti").setAttribute("popped", "");
+      }, 0);
+    });
   }
 
   static get styles() {
@@ -36,15 +85,26 @@ export class counterAp extends DDDSuper(LitElement) {
         padding: 0;
         margin: 0;
       }
+      button:disabled {
+        background-color: gray;
+        cursor: not-allowed;
+      }
     `];
   }
 
   render() {
     return html`
-<div class="wrapper">
-  <div>${this.title}</div>
-  <slot></slot>
-</div>`;
+    <div class = "counter">
+            ${this.counter}
+        </div>
+        <div class = "button">
+            <button @click="${this.increase}" ?disabled="${this.counter == this.max}">+</button>
+            <button @click="${this.decrease}" ?disabled="${this.counter == this.min}">-</button>
+        </div>  
+    <div class="wrapper">
+      <div>${this.title}</div>
+      <slot></slot>
+    </div>`;
   }
 
   /**
